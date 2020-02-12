e868afba277a6c86d48800f67b7ec27c065b11c7
Qing Jiang <qing.j@qingj-m1551.prodege.com>
2020-02-12 10:05:58 -0800

    update

--- jenkinsdeploy.groovy
+++ jenkinsdeploy.groovy
@@ -2,8 +2,6 @@ import static groovy.io.FileType.FILES
 @NonCPS
 def inputParamsString(dir) {
  def list = []
-
- // If you don't want to search recursively then change `eachFileRecurse` -> `eachFile`
  dir.eachFileRecurse(FILES) {
   // Change `.properties` to the file extension you are interested in
   if (it.name.beginWith('terraform-aws')) {
@@ -14,19 +12,19 @@ def inputParamsString(dir) {
  list.join("\n")
 }
 
+def MODULE_LIST = inputParamsString(new File(pwd()))
+
 pipeline {
     agent {
         docker {
             image 'hashicorp/terraform:0.12.20'
             args '--entrypoint=""'
         }
-
-        def inputParams = inputParamsString(new File(pwd()))
-        def selectedProperty = input( id: 'tf_module', message: 'Choose module name', parameters: [ [$class: 'ChoiceParameterDefinition', choices: inputParams, description: 'terraform module', name: 'prop'] ])
-    
-        println "Property: $selectedProperty"
     }
 
+    parameters {
+        choice(name: 'module_name' , choices: MODULE_LIST, description: "module name")
+    }
 
     stages {
          stage('fmt') {
